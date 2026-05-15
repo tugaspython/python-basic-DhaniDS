1531a735a25efdc1ee7a571ef864d69c5f605f46
DhaniDS <dhanidwinawans12@gmail.com>
2026-05-15 17:04:08 +0700

    Update jawaban_mahasiswa.py

--- jawaban_mahasiswa.py
+++ jawaban_mahasiswa.py
@@ -1,17 +1,10 @@
-def tambah(a, b):
-  """Fungsi ini menerima dua angka dan mengembalikan hasil penjumlahannya."""
-  return a + b
+import re
 
-def kurang(a, b):
-  """Fungasi ini menerima dua angka dan mengembalikan hasil pengurangannya."""
-  return a - b
-
-def kali(a, b):
-  """Fungsi ini menerima dua angka dan mengembalikan hasil perkaliannya."""
-  return a * b
-
-def bagi(a, b):
-  """Fungsi ini menerima dua angka dan mengembalikan hasil pembagiannya. Jika pembaginya adalah 0, kembalikan string "Error: Pembagian dengan nol"."""
-  if b == 0:
-      return "Error: Pembagian dengan nol"
-  return a / b
+def validasi_password(password):
+    # Cek batas panjang karakter
+    if len(password) < 8 or len(password) > 20: 
+        return False
+        
+    # Satu pola Regex untuk mengecek huruf besar, angka, simbol, dan tanpa spasi
+    pattern = r"^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[^ ]+$"
+    return bool(re.match(pattern, password))
