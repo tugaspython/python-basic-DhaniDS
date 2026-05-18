2925e8a89b955774f9c9b426c900d8bc28fb362a
DhaniDS <dhanidwinawans12@gmail.com>
2026-05-18 16:18:50 +0700

    Update jawaban_mahasiswa.py

--- jawaban_mahasiswa.py
+++ jawaban_mahasiswa.py
@@ -5,6 +5,6 @@ def validasi_password(password):
     if len(password) < 8 or len(password) > 20: 
         return False
         
-    # Satu pola Regex untuk mengecek huruf besar, angka, simbol, dan tanpa spasi
+    # Satu pola Regex untuk mengecek huruf besar, angka, simbol, dan tanpa spas
     pattern = r"^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[^ ]+$"
     return bool(re.match(pattern, password))
