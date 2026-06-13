25591ec128dfba5ba5328ff6b85a1c8e5ccccb3d
DhaniDS <dhanidwinawans12@gmail.com>
2026-06-13 13:45:08 +0700

    Update test_password.py

--- test_password.py
+++ test_password.py
@@ -1,4 +1,3 @@
-# File: test_password.py
 from jawaban_mahasiswa import validasi_password
 
 def test_validasi_sukses():
@@ -6,7 +5,13 @@ def test_validasi_sukses():
     assert validasi_password("P@ssw0rd") == True
 
 def test_validasi_gagal():
-    assert validasi_password("lemah") == False # Kependekan
+    assert validasi_password("lemah") == False 
     assert validasi_password("TanpaAngka!") == False
     assert validasi_password("TanpaSimbol123") == False
     assert validasi_password("Ada Spasi123!") == False
+
+def test_validasi_big_o():
+    # Membuat password valid dengan panjang 50.000 karakter
+    # Contoh: "AAAAA..." (sebanyak 49.997 kali) + "1!b"
+    password_panjang = "A" * 49997 + "1!b"
+    assert validasi_password(password_panjang) == True
