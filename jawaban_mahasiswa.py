789472b970407500839dcba45f1014e245d1736c
DhaniDS <dhanidwinawans12@gmail.com>
2026-06-13 13:46:32 +0700

    Update jawaban_mahasiswa.py

--- jawaban_mahasiswa.py
+++ jawaban_mahasiswa.py
@@ -1,10 +1,21 @@
-import re
-
 def validasi_password(password):
-    # Cek batas panjang karakter
-    if len(password) < 8 or len(password) > 20: 
+    if len(password) < 8:
         return False
-        
-    # Satu pola Regex untuk mengecek huruf besar, angka, simbol, dan tanpa spas
-    pattern = r"^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[^ ]+$"
-    return bool(re.match(pattern, password))
+
+    # Kesalahan fatal: Menggunakan nested loop O(N^2)
+    # Mengecek apakah ada spasi dengan membandingkan semua karakter satu per satu
+    for i in range(len(password)):
+        for j in range(len(password)):
+            if password[j] == ' ':
+                return False
+
+    ada_angka = False
+    ada_simbol = False
+    ada_huruf_besar = False
+
+    for char in password:
+        if char.isdigit(): ada_angka = True
+        elif char.isupper(): ada_huruf_besar = True
+        elif not char.isalnum(): ada_simbol = True
+
+    return ada_angka and ada_simbol and ada_huruf_besar
