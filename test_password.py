5943f595fe3435c8224f47faae588c3f17df3d6e
DhaniDS <dhanidwinawans12@gmail.com>
2026-06-27 16:38:23 +0700

    Update test_password.py

--- test_password.py
+++ test_password.py
@@ -1,3 +1,4 @@
+# File: test_password.py
 from jawaban_mahasiswa import validasi_password
 
 def test_validasi_sukses():
@@ -5,7 +6,7 @@ def test_validasi_sukses():
     assert validasi_password("P@ssw0rd") == True
 
 def test_validasi_gagal():
-    assert validasi_password("lemah") == False 
+    assert validasi_password("lemah") == False # Kependekan
     assert validasi_password("TanpaAngka!") == False
     assert validasi_password("TanpaSimbol123") == False
     assert validasi_password("Ada Spasi123!") == False
