01ba046ccb3ecb05c233153be7b415dd52f3e6ee
DhaniDS <dhanidwinawans12@gmail.com>
2026-06-27 16:32:37 +0700

    Update jawaban_mahasiswa.py

--- jawaban_mahasiswa.py
+++ jawaban_mahasiswa.py
@@ -1,11 +1,6 @@
-1.	# File: test_password.py
-2.	from jawaban_mahasiswa import validasi_password
-3.	
-4.	def test_validasi_sukses():
-5.	assert validasi_password("PasswordKuat123!") == True
-6.	assert validasi_password("P@ssw0rd") == True
-7.	
-8.	def test_validasi_gagal():
-9.	assert validasi_password("lemah") == False
-10.	assert validasi_password("TanpaAngka!") == False
-11.	assert validasi_password("TanpaSimbol123") == False
+import re
+def validasi_password(password):
+  if len(password) < 8 or len(password) > 20: 
+return False	   
+pattern = r"^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[^ ]+$"
+return bool(re.match(pattern, password))
