8a135921b0badde6ca2885f75d5476d787016daf
DhaniDS <dhanidwinawans12@gmail.com>
2026-06-27 16:38:59 +0700

    Update jawaban_mahasiswa.py

--- jawaban_mahasiswa.py
+++ jawaban_mahasiswa.py
@@ -1,6 +1,8 @@
 import re
+
 def validasi_password(password):
   if len(password) < 8 or len(password) > 20: 
-return False	   
+    return False
+    
 pattern = r"^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[^ ]+$"
 return bool(re.match(pattern, password))
