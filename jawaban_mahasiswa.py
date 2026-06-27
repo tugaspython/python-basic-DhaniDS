adaf5da0cc467cdfea5b7bf277803333ea6d4137
DhaniDS <dhanidwinawans12@gmail.com>
2026-06-27 16:40:53 +0700

    Update jawaban_mahasiswa.py

--- jawaban_mahasiswa.py
+++ jawaban_mahasiswa.py
@@ -1,8 +1,10 @@
-import re
-
-def validasi_password(password):
-  if len(password) < 8 or len(password) > 20: 
-    return False
-    
-pattern = r"^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[^ ]+$"
-return bool(re.match(pattern, password))
+	import re
+	
+	def validasi_password(password):
+	# Cek batas panjang karakter
+	if len(password) < 8 or len(password) > 20: 
+return False
+	   
+	# Satu pola Regex untuk mengecek huruf besar, angka, simbol, dan tanpa spasi
+	pattern = r"^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[^ ]+$"
+	return bool(re.match(pattern, password))
